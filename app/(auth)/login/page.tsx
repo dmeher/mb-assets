d4b717e38661e9156c3acda7f07ce2189cf9be5a
Debendra Meher <dmeher1996@gmail.com>
2025-03-14 18:14:46 +0530

    .

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -39,13 +39,6 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="w-[100%] flex items-center justify-center">
-        <Image
-          alt="HeroUI hero Image"
-          src="./images/GroceryImage.png"
-          className="w-[100%] md:w-[300] lg:w-[300] xl:w-[300]"
-        />
-      </div>
       <div className="w-[100%] flex items-center justify-start">
         <div className="flex flex-col items-start justify-center">
           <div className="font-bold">Get your groceries with</div>
@@ -62,10 +55,7 @@ export default function LoginPage() {
           maxLength={10}
           minLength={10}
           startContent={
-            <Image
-              src="./images/IN.png"
-              className="w-[2rem] rounded-none"
-            />
+            <Image src="./images/IN.png" className="w-[2rem] rounded-none" />
           }
           className="w-[100%] tracking-widest"
           fullWidth
@@ -91,6 +81,13 @@ export default function LoginPage() {
         >
           Login
         </Button>
+      </div>
+      <div className="w-[100%] flex items-center justify-center pt-[2rem]">
+        <Image
+          alt="HeroUI hero Image"
+          src="./images/GroceryImage.png"
+          className="w-[100%] md:w-[300] lg:w-[300] xl:w-[300]"
+        />
       </div>
     </div>
   );
