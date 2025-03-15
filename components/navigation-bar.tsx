c198e3280d446612d96bbac095ac0746ea5bc773
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 14:27:10 +0530

    .

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -5,31 +5,31 @@ export default function NavigationBar() {
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-shop text-[1.75rem] text-secondary"></i>
         </div>
-        <div className="font-bold text-secondary text-[1rem]">Shop</div>
+        <div className="font-semibold text-secondary text-[1rem]">Shop</div>
       </div>
       <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-search text-[1.75rem] text-secondary"></i>
         </div>
-        <div className="font-bold text-secondary text-[1rem]">Explore</div>
+        <div className="font-semibold text-secondary text-[1rem]">Explore</div>
       </div>
       <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-basket text-[1.75rem] text-secondary"></i>
         </div>
-        <div className="font-bold text-secondary text-[1rem]">Cart</div>
+        <div className="font-semibold text-secondary text-[1rem]">Cart</div>
       </div>
       <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-heart text-[1.75rem] text-secondary"></i>
         </div>
-        <div className="font-bold text-secondary text-[1rem]">Favourite</div>
+        <div className="font-semibold text-secondary text-[1rem]">Favourite</div>
       </div>
       <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-person text-[1.75rem] text-secondary"></i>
         </div>
-        <div className="font-bold text-secondary text-[1rem]">Account</div>
+        <div className="font-semibold text-secondary text-[1rem]">Account</div>
       </div>
     </div>
   );
