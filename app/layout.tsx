0808a5931c31dff27cf7e4f07bb697f7e0e95dcd
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 09:10:05 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <head />
       <body className={"min-h-screen bg-background antialiased"}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex flex-col h-screen pt-[1rem]">
             <Navbar />
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
               {children}
