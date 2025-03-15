60540fc4732f716b64d05dcec1936c38c7bccde9
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 14:33:30 +0530

    .

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -19,12 +19,12 @@ export default function NavigationBar() {
         </div>
         <div className="font-semibold text-secondary text-[1rem]">Cart</div>
       </div>
-      <div className="flex flex-col justify-center items-center gap-[.125rem]">
+      {/* <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-heart text-[1.75rem] text-secondary"></i>
         </div>
         <div className="font-semibold text-secondary text-[1rem]">Favourite</div>
-      </div>
+      </div> */}
       <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-person text-[1.75rem] text-secondary"></i>
