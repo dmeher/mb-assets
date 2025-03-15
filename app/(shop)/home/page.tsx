277b7eace7d6fa11bd9a00511b3f0d61c2d09e0d
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 19:06:58 +0530

    .

--- app/(shop)/home/page.tsx
+++ app/(shop)/home/page.tsx
@@ -5,7 +5,7 @@ import { Input } from "@heroui/input";
 export default function Home() {
   return (
     <div className="h-screen">
-      <section className="flex flex-col items-center justify-center gap-4 px-6">
+      <section className="flex flex-col items-center justify-center gap-1 px-6">
         <div className="flex items-center justify-center gap-3 w-screen">
           <i className="bi bi-geo-alt-fill text-[1.5rem] text-invert"></i>
           <div className="font-bold">Chhepapali, Nuabasti</div>
