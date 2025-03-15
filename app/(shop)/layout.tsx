82ac962614fdceeb7584d691611502407c4fafe9
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 19:02:31 +0530

    .

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <div className="container flex-grow">
       {children}
-      <div className="fixed bottom-[0] w-screen shadow-[2px_-5px_25px_0px_rgba(85,_94,_88,_0.5)] rounded-t-[1rem] pt-[.5rem] px-[1rem] flex items-center justify-center">
+      <div className="sticky bottom-[0] w-screen shadow-[2px_-5px_25px_0px_rgba(85,_94,_88,_0.5)] rounded-t-[1rem] pt-[.5rem] px-[1rem] flex items-center justify-center">
         <NavigationBar />
       </div>
     </div>
