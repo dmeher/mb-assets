5f1696430d074f6fb5ab6336f15aa83f8a5eabde
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 14:50:09 +0530

    .

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,36 +1,112 @@
+"use client";
+
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
+import { useState } from "react";
+
 export default function NavigationBar() {
+  const [currentPage, setCurrentPage] = useState("shop");
+
   return (
     <div className="flex justify-around w-[100%] items-center">
-      <div className="flex flex-col justify-center items-center gap-[.125rem]">
-        <div className="flex items-center justify-center h-[2rem] w-[2rem]">
-          <i className="bi bi-shop text-[1.75rem] text-secondary"></i>
+      <Link className="flex flex-col justify-center items-center gap-[.125rem]">
+        <Button
+          className="flex items-center justify-center h-[2rem] w-[2rem] bg-transparent"
+          onPress={() => setCurrentPage("shop")}
+          isIconOnly
+          variant="flat"
+        >
+          <i
+            className={
+              "bi bi-shop text-[1.75rem] " +
+              (currentPage === "shop" ? "text-primary" : "text-black")
+            }
+          ></i>
+        </Button>
+        <div
+          className={
+            "font-semibold text-[1rem] " +
+            (currentPage === "shop" ? "text-primary" : "text-black")
+          }
+        >
+          Shop
         </div>
-        <div className="font-semibold text-secondary text-[1rem]">Shop</div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-[.125rem]">
-        <div className="flex items-center justify-center h-[2rem] w-[2rem]">
-          <i className="bi bi-search text-[1.75rem] text-secondary"></i>
+      </Link>
+      <Link className="flex flex-col justify-center items-center gap-[.125rem]">
+        <Button
+          className="flex items-center justify-center h-[2rem] w-[2rem] bg-transparent"
+          onPress={() => setCurrentPage("explore")}
+          isIconOnly
+          variant="flat"
+        >
+          <i
+            className={
+              "bi bi-search text-[1.75rem] " +
+              (currentPage === "explore" ? "text-primary" : "text-black")
+            }
+          ></i>
+        </Button>
+        <div
+          className={
+            "font-semibold text-[1rem] " +
+            (currentPage === "explore" ? "text-primary" : "text-black")
+          }
+        >
+          Explore
         </div>
-        <div className="font-semibold text-secondary text-[1rem]">Explore</div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-[.125rem]">
-        <div className="flex items-center justify-center h-[2rem] w-[2rem]">
-          <i className="bi bi-basket text-[1.75rem] text-secondary"></i>
+      </Link>
+      <Link className="flex flex-col justify-center items-center gap-[.125rem]">
+        <Button
+          className="flex items-center justify-center h-[2rem] w-[2rem] bg-transparent"
+          onPress={() => setCurrentPage("cart")}
+          isIconOnly
+          variant="flat"
+        >
+          <i
+            className={
+              "bi bi-basket text-[1.75rem] " +
+              (currentPage === "cart" ? "text-primary" : "text-black")
+            }
+          ></i>
+        </Button>
+        <div
+          className={
+            "font-semibold text-[1rem] " +
+            (currentPage === "cart" ? "text-primary" : "text-black")
+          }
+        >
+          Cart
         </div>
-        <div className="font-semibold text-secondary text-[1rem]">Cart</div>
-      </div>
+      </Link>
       {/* <div className="flex flex-col justify-center items-center gap-[.125rem]">
         <div className="flex items-center justify-center h-[2rem] w-[2rem]">
           <i className="bi bi-heart text-[1.75rem] text-secondary"></i>
         </div>
         <div className="font-semibold text-secondary text-[1rem]">Favourite</div>
       </div> */}
-      <div className="flex flex-col justify-center items-center gap-[.125rem]">
-        <div className="flex items-center justify-center h-[2rem] w-[2rem]">
-          <i className="bi bi-person text-[1.75rem] text-secondary"></i>
+      <Link className="flex flex-col justify-center items-center gap-[.125rem]">
+        <Button
+          className="flex items-center justify-center h-[2rem] w-[2rem] bg-transparent"
+          onPress={() => setCurrentPage("account")}
+          isIconOnly
+          variant="flat"
+        >
+          <i
+            className={
+              "bi bi-person text-[1.75rem] " +
+              (currentPage === "account" ? "text-primary" : "text-black")
+            }
+          ></i>
+        </Button>
+        <div
+          className={
+            "font-semibold text-[1rem] " +
+            (currentPage === "account" ? "text-primary" : "text-black")
+          }
+        >
+          Account
         </div>
-        <div className="font-semibold text-secondary text-[1rem]">Account</div>
-      </div>
+      </Link>
     </div>
   );
 }
