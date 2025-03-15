9f7bee22b286df076242e35b8df218014982962c
Debendra Meher <dmeher1996@gmail.com>
2025-03-15 18:12:56 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+"use client";
 
+import { Link } from "@heroui/link";
+import { button as buttonStyles } from "@heroui/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/home");
+    }, 500);
+  }, []);
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 px-6">
       <div className="inline-block max-w-xl text-center justify-center">
