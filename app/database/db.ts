5dc7d9b727add8fcaa5398e6a6b7e28e09c4f064
Debendra Meher <dmeher1996@gmail.com>
2025-03-16 17:31:15 +0530

    .

--- app/database/db.ts
+++ app/database/db.ts
@@ -5,8 +5,7 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT ?? "5432", 10),
-  ssl: true,
+  port: parseInt(process.env.DB_PORT ?? "5432", 10)
 });
 
 // export const pool = new Pool({
