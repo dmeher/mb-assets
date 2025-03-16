2a1eaa6433e6b3965a96193a22ee98c91833e38c
Debendra Meher <dmeher1996@gmail.com>
2025-03-16 17:14:44 +0530

    .

--- app/database/db.ts
+++ app/database/db.ts
@@ -1,10 +1,19 @@
 import { Pool } from "pg";
 
+// export const pool = new Pool({
+//   user: (process.env.DB_USER ?? process.env.POSTGRES_USER),
+//   host: (process.env.DB_HOST ?? process.env.POSTGRES_HOST),
+//   database: (process.env.DB_DATABASE ?? process.env.POSTGRES_DATABASE),
+//   password: (process.env.DB_PASSWORD ?? process.env.POSTGRES_PASSWORD),
+//   port: parseInt(process.env.DB_PORT ?? "5432", 10),
+//   ssl: true,
+// });
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
   port: parseInt(process.env.DB_PORT ?? "5432", 10),
   ssl: true,
 });
