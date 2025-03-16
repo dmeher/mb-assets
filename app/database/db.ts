75dd1d4e7eb1f7bf3376c296f520d9fc7494b7f2
Debendra Meher <dmeher1996@gmail.com>
2025-03-16 17:37:26 +0530

    .

--- app/database/db.ts
+++ app/database/db.ts
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT ?? "5432", 10)
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // port: parseInt(process.env.DB_PORT ?? "5432", 10)
+  connectionString: process.env.POSTGRES_URL
 });
 
 // export const pool = new Pool({
