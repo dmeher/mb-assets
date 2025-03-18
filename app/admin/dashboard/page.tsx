28f13cdee0550af6fc02429f056a951647865d78
Debendra Meher <dmeher1996@gmail.com>
2025-03-18 10:16:37 +0530

    .

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { Link } from "@heroui/link";
+
 export default function DashboardPage() {
-  return <div className="flex flex-col h-screen px-6">Admin Dashboard!</div>;
+  return (
+    <div className="flex flex-col justify-start items-center h-screen px-6">
+      <div>Admin Dashboard!</div>
+      <div>
+        <Link href="/admin/products">Product Catalog</Link>
+      </div>
+    </div>
+  );
 }
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductPage() {
+  return (
+    <div className="flex flex-col justify-start items-center h-screen px-6">
+      <div>Product Catalog!</div>
+    </div>
+  );
+}
